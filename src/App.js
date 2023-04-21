37362672b4203b5a3a2293bf343ea8c6d776d4f9
nao-kichi <namnam3225@gmail.com>
2023-04-21 13:02:56 +0900

    ストップウォッチを作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+const time = document.getElementById('time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let startTime;
+let stopTime = 0;
+let timeoutID;
+
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const hours = String(currentTime.getUTCHours()).padStart(2, '0');
+  const minute = String(currentTime.getMinutes()).padStart(2, '0');
+  const second = String(currentTime.getSeconds()).padStart(2, '0');
+  const microsecond = String(currentTime.getMilliseconds()).padStart(3, '0');
+
+  time.textContent = `${hours}:${minute}:${second}.${microsecond}`;
+  timeoutID = setTimeout(displayTime, 10);
 }
 
-export default App;
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = true;
+  startTime = Date.now();
+  displayTime();
+});
+
+stopButton.addEventListener('click', function () {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += (Date.now() - startTime);
+});
+
+resetButton.addEventListener('click', function () {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+  time.textContent = '00:00:00.000';
+  stopTime = 0;
+});
